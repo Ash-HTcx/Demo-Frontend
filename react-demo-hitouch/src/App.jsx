a7e32b6d57cb5ae1c249cea8238d256716ed70f9
Shamayil Ahmad <shamayil.amd@gmail.com>
2025-10-01 13:01:24 +0530

    modified some changes

--- react-demo-hitouch/src/App.jsx
+++ react-demo-hitouch/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>This is awesome and easy</h1>
+      <h1>This is awesome and easy and funn</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
