7e0b15bdacf72c0f415a0a5c34bbc2f29a21bd65
Shamayil Ahmad <shamayil.amd@gmail.com>
2025-10-01 13:04:44 +0530

    some changes to color and all

--- react-demo-hitouch/src/App.jsx
+++ react-demo-hitouch/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>This is awesome and easy. I am working on frontend-two branch</h1>
+      <h1>This work is being done on Frontend Three. </h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
